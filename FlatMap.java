4b888e87d29b310c613aadad4f21babc79f53ad8
Shadiyah Mangru <shadiyahmangru@gmail.com>
2019-03-18 15:40:09 -0400

    Update FlatMap.java

--- FlatMap.java
+++ FlatMap.java
@@ -8,7 +8,7 @@ public class FlatMap{
 
 	Path file = new File("C:\\Users\\593476\\Desktop\\Java Programs\\TabularHockeyData\\2019WSHStatsREV.txt").toPath();
 
-	public static BiPredicate<String, String[]> keepMatches = (input, matchThisArray) -> {
+	public BiPredicate<String, String[]> keepMatches = (input, matchThisArray) -> {
 		if(matchThisArray.length==2){
 			if(input.contains(matchThisArray[0]) | input.contains(matchThisArray[1])){
 				return true;	
@@ -25,10 +25,18 @@ public class FlatMap{
 	
 	public void getMatchCount(String matchCategory, String... matchThis){
 	try{
-		long matches = Files.lines(file)
+		long matches = 
+		Files.lines(file)
 		.flatMap(line -> Stream.of(line.split("\n"))) //reads in data line-by-line
 		.filter(inputString -> keepMatches.test(inputString, matchThis))
-		.peek(match -> System.out.println(match))
+		.peek(match -> {String[] matchArray = match.split(":");
+			String ln = matchArray[1].trim();
+			int pipeIndexLN = ln.indexOf("|");
+			String position = matchArray[2].trim();
+			int pipeIndexP = position.indexOf("|");
+			HockeyPlayer hp = new HockeyPlayer(ln.substring(0, pipeIndexLN), position.substring(0, pipeIndexP), Integer.parseInt(matchArray[3].trim()), "WSH");
+			System.out.println(hp);
+			})
 		.count();
 	
 		System.out.println("\n# of " + matchCategory + ": " + matches);
@@ -38,6 +46,9 @@ public class FlatMap{
 	}
 	}
 
+	Supplier<String> dataTableHeader  = ()-> String.format("| %-4s | %-15s | %-4s | %-11s |", "TEAM", "PLAYER", "#", "POSITION") +
+			"\n-----------------------------------------------";
+	
 	public void mainMenu(){
 		System.out.println("\n\t**********************************************************");
 		System.out.println(String.format("%1s %-53s %2s", "\t*", "***** Welcome to Hockey Data Filtering Wizard! *****", "*"));
@@ -64,31 +75,38 @@ public class FlatMap{
 			System.exit(0);
 		}
 		else if(userInput.equals("7")){
-			System.out.println("*** Filter Selection: Goalies ***");
+			System.out.println("*** Filter Selection: Goalies ***\n");
+			System.out.println(dataTableHeader.get());
 			getMatchCount("Goalies", "Goalie");
 		}
 		else if(userInput.equals("6")){
-			System.out.println("*** Filter Selection: Defensemen ***");
+			System.out.println("*** Filter Selection: Defensemen ***\n");
+			System.out.println(dataTableHeader.get());
 			getMatchCount("Defensemen", "Defense");
 		}
 		else if(userInput.equals("5")){
-			System.out.println("*** Filter Selection: Forwards ***");
+			System.out.println("*** Filter Selection: Forwards ***\n");
+			System.out.println(dataTableHeader.get());
 			getMatchCount("Forwards", "Wing", "Center");
 		}
 		else if(userInput.equals("4")){
-			System.out.println("*** Filter Selection: Wingers ***");
+			System.out.println("*** Filter Selection: Wingers ***\n");
+			System.out.println(dataTableHeader.get());
 			getMatchCount("Wingers", "Wing");
 		}
 		else if(userInput.equals("3")){
-			System.out.println("*** Filter Selection: Right-Wingers ***");
+			System.out.println("*** Filter Selection: Right-Wingers ***\n");
+			System.out.println(dataTableHeader.get());
 			getMatchCount("Right-Wingers", "Right");
 		}
 		else if(userInput.equals("2")){
-			System.out.println("*** Filter Selection: Left-Wingers ***");
+			System.out.println("*** Filter Selection: Left-Wingers ***\n");
+			System.out.println(dataTableHeader.get());
 			getMatchCount("Left-Wingers", "Left");
 		}
 		else{
-			System.out.println("*** Filter Selection: Centers ***");
+			System.out.println("*** Filter Selection: Centers ***\n");
+			System.out.println(dataTableHeader.get());
 			getMatchCount("Centers", "Center");
 		}
 		mainMenu();
